eff08d529fce37e7c66c3ee9d2e0d5e98485f7c6
a20956 <51470960+a20956@users.noreply.github.com>
2019-07-12 11:43:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def callback():
     userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
     if (userText == 'สวัสดี') :
-        sendText(user,'แฮร์สวัสดี')
+        sendText(user, 'แฮร์สวัสดี')
     elif (userText == 'ไอ้บ้า') :
         sendText(user,'มึงสิบ้า555+')
     elif (userText == 'สวยนะ') :
